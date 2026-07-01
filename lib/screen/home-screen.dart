d81d09fd7631b18cae57ef324c85f2fddbafed62
bravoboy <abubakar.siddik2146@gmail.com>
2026-07-01 23:58:55 +0100

    Implemented Two new widget on the home screen: To show current Salah start time and Time Remaining, second, upcoming lists of salah based on the current salah.
    all temporary placements.
    imported a new font from Google font

--- lib/screen/home-screen.dart
+++ lib/screen/home-screen.dart
@@ -20,20 +20,194 @@ class HomeScreen extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             title: Center(
-              child: Text('Salah App'),
+              child: Text('Salah App',
+              style: TextStyle(
+                fontSize: 32,
+                letterSpacing: 32 * 0.17,
+                color: Color(0xFF32BB56),
+              ),
+              ),
             ),
           ),
-          body: Column(
-            children: [
-              Center(
-                child:
-                Text('${salahName}'),
+          body: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+            child: Center(
+          child: Padding(
+              padding: EdgeInsets.all(16),
+        child: Text('${salahName}',
+          style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 20 * 0.17
+          ),
+        ),
+      )
+            ),
+
+    ),
+              SliverToBoxAdapter(
+                child: _salahTime()
+              ),
+              SliverFillRemaining(
+                  child: _upcomingSalah()
               ),
             ],
           )
+
+
       ),
     );
 
   }
-  
+
+  Widget _salahTime(){
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 500),
+      padding: EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                const Text(
+                    'Start Time',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    letterSpacing: 2,
+                    fontSize: 14,
+                  ),
+                ),
+
+                  const SizedBox(height: 8),
+                  _buildTimeCard('04:32:40 PM'), //Temporary placement
+          ],
+            ),
+            ),
+
+            const SizedBox(width: 20),
+
+            Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Time Left',
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                      letterSpacing: 2,
+                        fontSize: 14
+                      ),
+                    ),
+
+                    const SizedBox(height: 8),
+                    _buildTimeCard('05:32:40 PM'), //Temporary placement
+                  ],
+                ),
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+
+
+  //*!! temporary placeholders !!*//
+
+  /*
+  TODO: needs to implement a countdown timer from the start time to the end time
+
+   */
+
+  Widget _buildTimeCard(String time) {
+  return Container(
+    width: double.infinity,
+    padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+    decoration: BoxDecoration(
+      color: Colors.grey,
+        borderRadius: BorderRadius.circular(20)
+    ),
+
+    child: Center(
+      child: Text(
+          time,
+        style: GoogleFonts.montserrat(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+          color: Colors.black87
+        ),
+      ),
+    ),
+  );
+  }
+
+
+  /*
+  TODO: needs to implement a list of upcoming salahs, based on the current Salah
+
+   */
+
+  Widget _upcomingSalah(){
+     List<Salah> salahList() => Salah.values;
+    return Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 500),
+      padding: EdgeInsets.symmetric(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+
+          children: [
+            for(final salah in salahList())...[
+              _buildSalahRow(salah, '06:32:40 PM'),
+              if (salah != salahList().last)
+                const Divider(height: 1,color: Colors.black12,thickness: 0.5),
+            ]
+          ],
+        ),
+      ),
+
+    )
+    );
+  }
+
+
+  /*
+  Shows a list of salahs with their times and names
+   */
+
+  Widget _buildSalahRow(Salah salah, String time){
+    return Padding(
+        padding: const EdgeInsets.symmetric(),
+      child: Row(
+        children: [
+          Text(salah.displaySalahName,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Colors.black38
+          ),
+          ),
+
+          const Spacer(),
+
+          Text(time,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            color: Colors.black38
+          ),
+          )
+
+        ],
+      ),
+    );
+  }
 }
